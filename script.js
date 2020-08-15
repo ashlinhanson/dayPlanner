cd55573f9c18168208892d8e56412c95f30dcfdf
ashlinhanson <ashlinhanson@gmail.com>
2020-08-15 18:18:34 -0500

    html/css styling containers and buttons, some pseudocode for jQuery

--- script.js
+++ script.js
@@ -2,7 +2,34 @@ $(document).ready(function () {
     //displaying the date and time at the top in the header of the page
 //the function to create the date and time for the header
 function displayDate() {
-    $("#currentDay").html(moment().format("MM DD YYYY, h:mm a"));
+    $("#currentDay").html(moment().format("LLLL"));
 } setInterval(displayDate, 1000);
-}
-)
+})
+
+
+
+
+$(".saveBtn").on("click", function(){
+    
+
+})
+
+localStorage.setItem$("#input9")
+
+
+
+
+// to set local storage
+// localStorage.setItem('data', JSON.stringify(data));
+
+//to get local storage
+// var data = JSON.parse(localStorage.getItem('data));
+
+//to delete local storage
+// localStorage.removeItem('data');
+
+//need to add eventlisteners to buttons to save user input and append it to the timeblock
+//need color changes based on future or past.
+
+
++
